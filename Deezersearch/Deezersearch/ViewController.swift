939b6f11a4c2caea1d8ccc75f9e97e5aefe5810f
amgh <amgh@netlight.com>
2017-10-16 16:32:35 +0200

    Initial commit

---
+++ Deezersearch/Deezersearch/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Deezersearch
+//
+//  Created by Amir Ghoreshi on 16/10/2017.
+//  Copyright © 2017 Amir Ghoreshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Deezer-search
-Search in deezer.com for artists, albums and tracks. 
+
+Search in deezer.com for artists, albums and tracks.
+
+`Swift 4.0`
+No `Storyboard` or `.xib`
+
+
+See https://developers.deezer.com/api
+
+
